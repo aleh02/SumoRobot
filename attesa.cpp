cfa67d446276e671330b8fe85b6746b728a92bb9
Alessandro Han <81700194+aleh02@users.noreply.github.com>
2025-11-18 00:52:53 +0100

    Update attesa.cpp

--- attesa.cpp
+++ attesa.cpp
@@ -1,27 +1,37 @@
-#include <arduino.h>
+#include <Arduino.h>
 #include "define_global.h"
 
+// Effetto countdown sui LED prima dell'avvio
 void attesa(byte *flag_attesa)
 {
   *flag_attesa = 1;
-  digitalWrite(LED_1, 1);
+
+  const byte leds[] = {LED_1, LED_2, LED_3, LED_4};
+
+  // Accendi a "corsa" i LED uno alla volta
+  for (byte i = 0; i < 4; i++)
+  {
+    // Spegni tutti
+    for (byte j = 0; j < 4; j++)
+    {
+      digitalWrite(leds[j], LOW);
+    }
+
+    // Accendi quello giusto
+    digitalWrite(leds[i], HIGH);
+    delay(1000);
+  }
+
+  // Tutti ON per 1 secondo
+  for (byte i = 0; i < 4; i++)
+  {
+    digitalWrite(leds[i], HIGH);
+  }
   delay(1000);
-  digitalWrite(LED_2, 1);
-  digitalWrite(LED_1, 0);
-  delay(1000);
-  digitalWrite(LED_3, 1);
-  digitalWrite(LED_2, 0);
-  delay(1000);
-  digitalWrite(LED_4, 1);
-  digitalWrite(LED_3, 0);
-  delay(1000);
-  digitalWrite(LED_1, 1);
-  digitalWrite(LED_2, 1);
-  digitalWrite(LED_3, 1);
-  digitalWrite(LED_4, 1);
-  delay(1000);
-  digitalWrite(LED_1, 0);
-  digitalWrite(LED_2, 0);
-  digitalWrite(LED_3, 0);
-  digitalWrite(LED_4, 0);
+
+  // Tutti OFF
+  for (byte i = 0; i < 4; i++)
+  {
+    digitalWrite(leds[i], LOW);
+  }
 }
