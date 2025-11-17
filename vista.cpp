6f939b8f4aeb292605e1b003929c56937465fe40
Alessandro Han <81700194+aleh02@users.noreply.github.com>
2025-11-18 00:56:13 +0100

    Update vista.cpp

--- vista.cpp
+++ vista.cpp
@@ -1,39 +1,38 @@
-#include <arduino.h>
+#include <Arduino.h>
 #include "define_global.h"
 #include "motore.h"
 
+#define SOGLIA_SHARP 400
+
+// Usa gli Sharp per "vedere" l'avversario e decidere come muoversi
 void vista(void)
 {
-  int sh_sx = analogRead(A2);
-  int sh_dx = analogRead(A3);
-  if (sh_sx > 400 && sh_dx > 400)
+  int sh_sx = analogRead(SHSX);
+  int sh_dx = analogRead(SHDX);
+
+  // LED di stato: ad esempio LED_3 acceso quando stiamo "cercando"
+  digitalWrite(LED_4, LOW);
+  digitalWrite(LED_3, HIGH);
+
+  if (sh_sx > SOGLIA_SHARP && sh_dx > SOGLIA_SHARP)
   {
-    digitalWrite(LED_4, 0);
-    digitalWrite(LED_3, 0);
-    digitalWrite(LED_3, 1);
+    // avversario davanti → vai dritto
     avanti();
   }
-  else if (sh_dx > 400)
+  else if (sh_dx > SOGLIA_SHARP)
   {    
-    digitalWrite(LED_4, 0);
-    digitalWrite(LED_3, 0);
-    digitalWrite(LED_3, 1);
+    // avversario verso destra
     destra();
   }
-  else if (sh_sx > 400)
+  else if (sh_sx > SOGLIA_SHARP)
   {
-    digitalWrite(LED_4, 0);
-    digitalWrite(LED_3, 0);
-    digitalWrite(LED_3, 1);
+    // avversario verso sinistra
     sinistra();
   }
   else
   {
-    digitalWrite(LED_4, 0);
-    digitalWrite(LED_3, 0);
-    digitalWrite(LED_3, 1);
-    avanti();
+    // nessun avversario rilevato: gira finché non trova l'avversario
+    //avanti();
+    rot_destra(); // o rot_sinistra();
   }
-
 }
-
